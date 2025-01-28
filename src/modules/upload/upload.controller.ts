4278deb716937b08301d52bedaa0dd70b92d31d2
Orkhan Haji <orkhan.ha@voicespin.com>
2025-01-28 21:39:29 +0400

    added cloudinary, added vercel configuration

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -10,7 +10,7 @@ import { AuthGuard } from 'src/guards/auth.guard';
 import { Roles } from 'src/shared/decorator/role.decorator';
 import { UserRole } from '../user/user.types';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
+import { diskStorage, memoryStorage } from 'multer';
 import { extname, join } from 'path';
 
 import * as crypto from 'crypto';
@@ -26,12 +26,15 @@ import {
   ApiProperty,
 } from '@nestjs/swagger';
 import { FileUploadDto } from './dto/upload-file.dto';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
 @UseGuards(AuthGuard)
 @Roles(UserRole.ADMIN, UserRole.CONTENT_MANAGER)
 @ApiBearerAuth()
 export class UploadController {
+  constructor(private uploadService: UploadService) {}
+
   @Post()
   @ApiConsumes('multipart/form-data')
   @ApiBody({ type: FileUploadDto })
@@ -71,5 +74,36 @@ export class UploadController {
       message: 'File is uploaded successfully',
       url: `/uploads/${file.filename}`,
     };
+  }
+
+  @Post('cloud')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: FileUploadDto })
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: memoryStorage(),
+      fileFilter(req, file, callback) {
+        let ext = extname(file.originalname).toLowerCase().slice(1);
+        let mimeType = file.mimetype;
+
+        if (
+          !ALLOWED_IMAGE_MIME_TYPES.includes(mimeType) ||
+          !ALLOWED_IMAGE_EXTENSIONS.includes(ext)
+        ) {
+          return callback(
+            new BadRequestException('File format or type is not suitable'),
+            false,
+          );
+        }
+
+        callback(null, true);
+      },
+      limits: {
+        fileSize: 5e6,
+      },
+    }),
+  )
+  uploadToCloud(@UploadedFile('file') file: Express.Multer.File) {
+    return this.uploadService.uploadToCloud(file);
   }
 }
